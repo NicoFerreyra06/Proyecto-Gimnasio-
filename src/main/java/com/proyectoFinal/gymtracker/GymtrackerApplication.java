801b2fe25a958701aa08ce12c230e22dbfc6169f
nicolasferreyramdp-code <nicolasferreyramdp@gmail.com>
2026-04-27 11:32:15 -0300

    Prueba sout

--- src/main/java/com/proyectoFinal/gymtracker/GymtrackerApplication.java
+++ src/main/java/com/proyectoFinal/gymtracker/GymtrackerApplication.java
@@ -2,12 +2,15 @@ package com.proyectoFinal.gymtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class GymtrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymtrackerApplication.class, args);
+
+		System.out.println("HOLa");
 	}
 
 }
