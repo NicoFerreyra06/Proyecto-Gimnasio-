2dc10d06f90396214d39457a39f0faf5bd006535
nicolasferreyramdp-code <nicolasferreyramdp@gmail.com>
2026-04-28 11:39:38 -0300

    Limpiar caché para aplicar.gitignore

--- src/main/java/com/proyectoFinal/gymtracker/GymtrackerApplication.java
+++ src/main/java/com/proyectoFinal/gymtracker/GymtrackerApplication.java
@@ -2,7 +2,6 @@ package com.proyectoFinal.gymtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class GymtrackerApplication {
