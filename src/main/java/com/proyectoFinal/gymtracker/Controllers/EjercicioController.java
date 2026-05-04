d4463cf8e3b436f172bdf7d667795f6b31157d7d
valentin <caballarovalentin@gmail.com>
2026-05-04 15:53:12 -0300

    service y controller ejercicio arreglado

--- src/main/java/com/proyectoFinal/gymtracker/Controllers/EjercicioController.java
+++ src/main/java/com/proyectoFinal/gymtracker/Controllers/EjercicioController.java
@@ -17,29 +17,30 @@ import java.util.List;
 public class EjercicioController {
 
     private final EjercicioService ejercicioService;
+
     @PostMapping
-    public ResponseEntity<Ejercicio> addEjercicio(@RequestBody EjercicioRequest ejercicio){
+    public ResponseEntity<Ejercicio> addEjercicio(@RequestBody EjercicioRequest ejercicio) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ejercicioService.addEjercicio(ejercicio));
     }
 
     @PostMapping("/lote")
-    public ResponseEntity<List<Ejercicio>> addEjercicios(@RequestBody List<EjercicioRequest> ejercicios){
+    public ResponseEntity<List<Ejercicio>> addEjercicios(@RequestBody List<EjercicioRequest> ejercicios) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ejercicioService.addEjercicios(ejercicios));
     }
 
-    @DeleteMapping("/{idMusculo}")
-    public ResponseEntity<Void> deleteEjercicio(@PathVariable Long idMusculo){
-        ejercicioService.deleteEjercicio(idMusculo);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/{idMusuclo}")
-    public ResponseEntity<Ejercicio> getEjercicio(@PathVariable Long idEjercicio){
-        return ResponseEntity.ok(ejercicioService.getEjercicioById(idEjercicio));
+    @GetMapping("/{idEjercicio}")
+    public ResponseEntity<Ejercicio> getEjercicio(@PathVariable Long idEjercicio) {
+        return ResponseEntity.ok(ejercicioService.getById(idEjercicio));
     }
 
     @GetMapping
-    public ResponseEntity<List<Ejercicio>> getAllEjercicio(){
-        return ResponseEntity.status(HttpStatus.OK).body(ejercicioService.getEjercicios());
+    public ResponseEntity<List<Ejercicio>> getAllEjercicios() {
+        return ResponseEntity.ok(ejercicioService.getAll());
+    }
+
+    @DeleteMapping("/{idEjercicio}")
+    public ResponseEntity<Void> deleteEjercicio(@PathVariable Long idEjercicio) {
+        ejercicioService.deleteEjercicio(idEjercicio);
+        return ResponseEntity.noContent().build();
     }
 }
