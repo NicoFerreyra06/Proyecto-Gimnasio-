6b6adc812b6346c817d676051e8c70b1d03973b3
Juanfran06 <juanmarzoni@gmail.com>
2026-05-07 09:02:16 -0300

    ejercicio response

--- src/main/java/com/proyectoFinal/gymtracker/DTO/Response/EjercicioResponse.java
+++ src/main/java/com/proyectoFinal/gymtracker/DTO/Response/EjercicioResponse.java
@@ -1,4 +1,18 @@
 package com.proyectoFinal.gymtracker.DTO.Response;
 
+import com.proyectoFinal.gymtracker.Modelo.Musculo;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EjercicioResponse {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private List<Musculo> musculosPrincipales;
+    private List<Musculo> musculosSecundarios;
 }
